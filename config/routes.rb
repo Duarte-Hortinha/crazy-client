b31a1d8f04275cb162d12cefda995e2b944494a7
Andre <andre.luiz.bianco@gmail.com>
2024-06-04 12:29:47 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :restaurants
-  resources :restaurants, only: [:index]
+  resources :restaurants, only: [:index, :show]
+  resources :clients, except: [:destroy] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, except: [:new, :create] do
+    resources :reviews, except: [:destroy]
+  end
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
