bec40019ea3f6be5ef3516640ec66440b0556918
Alexandru Sandu <jokes.01.bidders@icloud.com>
2024-06-04 16:36:10 +0100

    show-client

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Client.destroy_all
+Restaurant.destroy_all
+
+Client.create([
+  {
+    first_name: "John",
+    last_name: "Doe",
+    phone_number: "555-1234",
+    ccr: 3.5
+  },
+  {
+    first_name: "Jane",
+    last_name: "Smith",
+    phone_number: "555-5678",
+    ccr: 4.9
+  },
+  {
+    first_name: "Alice",
+    last_name: "Johnson",
+    phone_number: "555-8765",
+    ccr: 2.2
+  },
+  {
+    first_name: "Bob",
+    last_name: "Brown",
+    phone_number: "555-4321",
+    ccr: 3.9
+  }
+])
+
+Restaurant.create(
+  email: "info@gourmetbistro.com",
+  password: "1234567",
+  name: "Gourmet Bistro",
+  ccr_acceptance: 2.5,
+  capacity: 100
+)
