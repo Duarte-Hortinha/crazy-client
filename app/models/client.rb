5b2c24e4e5bf993b10419575537ddfb6422b5588
Duarte Hortinha <duarte.hortinha@protonmail.com>
2024-06-04 18:00:08 +0100

    validation-client-not-working

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,7 @@
 class Client < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
+  validates :first_name, :last_name, :phone_number, presence: true, uniqueness: true
+  # validates :ccr, numericality: { in: 0.0..5.0 }
+  validates :ccr, presence: true, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 5.0 }
 end
