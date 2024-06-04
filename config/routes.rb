befee47316a5a55e56bfee47fd28872aa6ddccfc
Duarte Hortinha <duarte.hortinha@protonmail.com>
2024-06-04 18:21:01 +0100

    small-fix-update-validations

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :clients, except: [:destroy] do
     resources :bookings, only: [:new, :create]
   end
+
   resources :bookings, except: [:new, :create] do
     resources :reviews, except: [:destroy]
   end
+  
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'search', to: 'clients#index', as: :search
