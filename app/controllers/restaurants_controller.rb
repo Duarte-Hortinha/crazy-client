e772e239348dd81f3b1add35d1c37d99cfb72369
Andre <andre.luiz.bianco@gmail.com>
2024-06-05 18:10:06 +0100

    cloudinary unfinished

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,21 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def update
+    @restaurant = @restaurant.current_user
+
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "Restaurant info succesfully updated!"
+      redirect_to root_path
+    else
+      render action: :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:ccr_acceptance, :email, :name, :capacity, :photo)
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,6 @@ class Restaurant < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one_attached :photo
   has_many :bookings, dependent: :destroy
 end
