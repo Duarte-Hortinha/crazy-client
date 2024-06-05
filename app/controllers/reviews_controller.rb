3ed217bce00a5fa4ca2c2d5b9fd6a107ebb86b4d
Oliver Attolini <attolini.oliver@gmail.com>
2024-06-05 16:32:10 +0100

    not working dont pull

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,5 +14,9 @@ class ReviewsController < ApplicationController
     end
   end
 
+  private
 
+  def review_params
+    params.require(:review).permit(:punctuality, :comment, :attendance) # added photo because of cloudinary
+  end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
-pin "flatpickr" # @4.6.13
+pin "turbo" # @1.13.3
+pin "child_process" # @2.0.1
+pin "fs" # @2.0.1
+pin "path" # @2.0.1
+pin "process" # @2.0.1
+pin "stimulus"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
