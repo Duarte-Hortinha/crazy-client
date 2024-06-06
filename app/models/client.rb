767e954feed6b2b75485992c1fa2ad9698e38e6a
Alexandru Sandu <jokes.01.bidders@icloud.com>
2024-06-06 15:30:39 +0100

    ccr-score

--- app/models/client.rb
+++ app/models/client.rb
@@ -7,3 +7,11 @@ class Client < ApplicationRecord
   # validates :ccr, numericality: { in: 0.0..5.0 }
   # validates :ccr, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 5.0 }
 end
+
+
+
+
+
+# grep the review variables (punctuality and attendance)
+# change the ccr score acording with those variables
+# display that ccr score
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :booking
   validates :comment, length: { in: 5..200 }
+
+  # after_save :set_ccr
+
+  # def set_ccr
+  #   booking = Booking.find(self.booking.id)
+  #   client = Client.find(booking.client.id)
+  #   client.update(ccr: 3)
+  # end
 end
