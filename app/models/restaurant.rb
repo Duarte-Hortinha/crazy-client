b6f7a897217704ea97bab467d2c11444a6f2842e
Andre <andre.luiz.bianco@gmail.com>
2024-06-06 13:17:47 +0100

    navbar done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :photo
   has_many :bookings, dependent: :destroy
 end
