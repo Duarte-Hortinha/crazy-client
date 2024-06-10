032ae44e0a5dce24224b116bb93d3b3e6f1d43f0
Duarte Hortinha <duarte.hortinha@protonmail.com>
2024-06-10 15:51:51 +0100

    huge sweet alert commit

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
-import Swal from 'sweetalert2';
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@8/src/sweetalert2.js'
 
 // Connects to data-controller="alert"
 export default class extends Controller {
@@ -9,26 +9,32 @@ export default class extends Controller {
   connect() {
     // this.seeDetailsCrazyClientButtonTarget.addEventListener('click', this.showAlert);
     console.log("AlertController connected");
-    this.element.addEventListener('click', this.showAlert.bind(this));
+    //this.element.addEventListener('click', this.showAlert.bind(this));
   }
 
   showAlert(event) {
     event.preventDefault();  // Won't this not redirect ? ... problem corrected bellow
     Swal.fire({
-      title: "Warning: This client is bat-shmidt-crazy!!!",
-      width: 600,
-      padding: "3em",
-      color: "#716add",
-      background: "#fff url(https://sweetalert2.github.io/#examplesimages/trees.png)",
+      title: `⚠️Warning⚠️
+        This client is crazy!!!`,
+      width: 900,
+      padding: "10em",
+      color: "#fffff",
+      background: "#fff url(https://answerfirst.com/wp-content/uploads/2016/07/how-to-speak-to-angry-customers.jpg)",
       backdrop: `
         rgba(0,0,123,0.4)
-        url("https://sweetalert2.github.io/#examplesimages/nyan-cat.gif")
+        url(https://media.tenor.com/-AyTtMgs2mMAAAAi/nyan-cat-nyan.gif)
         left top
         no-repeat
       `
     }).then((result) => {
-      if (result.isConfirmed) {
-        window.location = this.element.href; // Navigate to the link if confirmed
+      if (result.value) {
+         const book = document.getElementById("book_now");
+         const clientId = book.dataset.clientId;
+         // Construct the path for the new booking
+         const bookingPath = `/clients/${clientId}/bookings/new`;
+         // Redirect to the new booking path
+         window.location.href = bookingPath;
       }
     });
 
