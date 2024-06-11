733f0701fcc284785d3bae38b959062b90130757
Alexandru Sandu <jokes.01.bidders@icloud.com>
2024-06-11 12:31:22 +0100

    pendig reviews

---
+++ app/javascript/controllers/countdown_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="countdown"
+export default class extends Controller {
+  static targets = ["clock"]
+
+  static values = {
+    time: String,
+  }
+
+  connect() {
+    this.startCountdown()
+  }
+
+  startCountdown() {
+    this.updateTimer()
+    this.interval = setInterval(() => {
+      this.updateTimer()
+    }, 1000)
+  }
+
+  updateTimer() {
+    const now = new Date();
+    const timeDeadline = new Date(this.timeValue);
+
+    const timeRemaining = timeDeadline - now;
+
+    if (timeRemaining <= 0) {
+      clearInterval(this.interval)
+      this.clockTarget.innerHTML = "Review period has ended."
+    } else {
+      const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000)
+
+      this.clockTarget.innerHTML = `${hours}h ${minutes}m ${seconds}s left`
+    }
+  }
+}
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,12 @@ class Booking < ApplicationRecord
   belongs_to :client
   validates :booking_start, :party_count, presence: true
   validates :party_count, numericality: { only_integer: true, in: 1..50 }
+
+  def review_deadline
+    booking_start + 72.hours
+  end
+
+  def review_active?
+    Time.current < review_deadline
+  end
 end
