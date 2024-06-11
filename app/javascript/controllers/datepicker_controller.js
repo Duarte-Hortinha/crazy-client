6e9e9502a93d283cbf82278ee9257e1ab5e057cb
fahed-bdidah <fahdbasesa@gmail.com>
2024-06-11 15:50:58 +0100

    date

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
 
+
 export default class extends Controller {
   static targets = [ "bookingStart" ]
 
@@ -8,8 +9,10 @@ export default class extends Controller {
     flatpickr(this.bookingStartTarget, {
       minDate: "today",
       altInput: true,
+      defaultDate: "today",
+      inline: true,
       "enableTime": true,
-      minuteIncrement: 15
+      minuteIncrement: 15,
     })
   }
 }
